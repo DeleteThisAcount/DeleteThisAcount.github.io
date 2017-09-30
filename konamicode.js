20c5ceea0048b2d32a9a06414b3ba12338d40a05
DeleteThisAcount <DeleteThisAcountScratch@gmail.com>
2017-09-30 17:08:57 +0200

    why no work
    u help pls

--- konamicode.js
+++ konamicode.js
@@ -1,4 +1,4 @@
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
-    document.getElementById('a').style.backgroundImage="url(images/banner.png)",
-  swal('ALL YOUR EASTER EGG BELONG TO US.');
+  swal('ALL YOUR EASTER EGG BELONG TO US.'),
+document.getElementById('a').style.backgroundImage="url(images/banner.png)";
 });
