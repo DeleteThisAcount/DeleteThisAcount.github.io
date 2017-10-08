8d121d7092e8ac0d3d708d74e30c093de8d32ad7
DeleteThisAcount <DeleteThisAcountScratch@gmail.com>
2017-10-08 11:03:19 +0200

    wont work
    100%

--- konamicode.js
+++ konamicode.js
@@ -1,8 +1,20 @@
 cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
-  swal(
-    'Congrats',
-    'You found an easter egg!',
-    'success'
-  ),
+  swal({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    type: 'success',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then(function () {
+    var colors = ['red', 'green', 'blue']; // Define Your colors here, can be html name of color, hex, rgb or anything what You can use in CSS
+    var active = 0;
+    setInterval(function(){
+        document.querySelector('body').style.background = colors[active];
+        active++;
+        if (active == colors.length) active = 0;
+    }, 30)
+  }),
 document.body.style.backgroundColor = "#a4c2f4";
 });
